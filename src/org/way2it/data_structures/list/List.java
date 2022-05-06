62fc3f56ca1c166416f5bbec2e098c70e0ee1c4c
DocAdler <101865537+DocAdler@users.noreply.github.com>
2022-05-06 22:54:17 +0300

    Решение

--- src/org/way2it/data_structures/list/List.java
+++ src/org/way2it/data_structures/list/List.java
@@ -1,4 +1,6 @@
-package org.way2it.data_structures.list;
+package List;
+
+import java.util.Objects;
 
 public class List {
 
@@ -14,30 +16,61 @@ public class List {
     // Should add new value to the end of the list, increment length and extend array by x2 if needed
     public void add(String value) {
         // TODO implement me
+        if (length == values.length) {
+            String[] temp = values;
+            this.values = new String[values.length+1];
+            System.arraycopy(temp,0,values, 0, temp.length);
+        }
+        values[length] = value;
+        length++;
     }
 
     // Should add new value at the specified index, moving other values to the right,
     // increment length and extend array by x2 if needed
     public void addAtIndex(String value, int index) {
         // TODO implement me
+        if (length == values.length) {
+            String[] temp = values;
+            this.values = new String[values.length+1];
+            System.arraycopy(temp,0,values, 0, temp.length);
+        }
+        for (int i = values.length-2; i >= index; i--) {
+            values[i+1] = values[i];
+        }
+        values[index] = value;
+        length++;
     }
 
     // Should return a value at specified index, or null if index is out of list bounds
     public String get(int index) {
         // TODO implement me
-        return null;
+        return values[index];
     }
 
     // Should return the index of the first occurrence of specified value in list
     // If the value does not exist - return -1
     public int indexOf(String value) {
         // TODO implement me
+        int index = 0;
+        for (String x : values) {
+            if (Objects.equals(value, x)) {
+                return index;
+            }
+            index++;
+        }
         return -1;
     }
 
     // Should remove the value at specified index, decrementing the length of this list and moving next values to the left
     public void remove(int index) {
         // TODO implement me
+        for (int i = index; i < values.length-1; i++) {
+            values[i] = values[i+1];
+        }
+        String[] temp = values;
+        this.values = new String[values.length-1];
+        System.arraycopy(temp,0,values, 0, values.length);
+        length--;
     }
 
     public int getLength() {
