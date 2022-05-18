40912e23794d0cf222ada68196c840ac2de75856
YK <enskynet@gmail.com>
2022-05-18 23:29:41 +0300

    BinaryTree is working.

--- src/org/way2it/data_structures/binary_tree/BinaryTree.java
+++ src/org/way2it/data_structures/binary_tree/BinaryTree.java
@@ -48,89 +48,72 @@ public class BinaryTree {
     // If value does not exist in this tree - return false
     public boolean remove(int value) {
         // TODO implement me
-        int temp = 0;
-        //int del;
         Node current = this.root;
-        Node find = this.root;
-        if (current.value == value) {
-            while (find.right != null) {
-                if (find.right == null) {
-                    find = null;
-                } else {
-                    find = find.right;
-                }
-                temp = find.value;
+        Node find = new Node();
+        while (current != null && current.value != value) {
+            find = current;
+            if (value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
             }
-            root.value = temp;
-            size--;
-            return true;
-        } else {
-            while (current != null) {
-                if (value < current.value) {
-                    if (current.left == null) {
-                        return false;
-                    }
-                    if (current.left.value == value) {
-
-
-                        current.left.value = 0;
-                        //root = current;
-                        size--;
-                        return true;
-                    } else {
-                        current = current.left;
-                    }
-                } else if (value > current.value) {
-                    if (current.right == null) {
-                        return false;
-                    }
-                    if (current.right.value == value) {
-                        if (current.right.right == null && current.right.left == null) {
-                            current.right = null;
-                        } else {
-                            while (find.right != null) {
-                                if (find.right == null) {
-                                    find = null;
-                                } else {
-                                    find = find.right;
-                                }
-                                temp = find.value;
-                               if(find.right == null && find.left==null){
-                                   find=null;
-                                   size--;
-                                   return true;
-                               }
-                            }
-                            current.right.value = temp;
-
-                        }
-                        size--;
-                        return true;
-
-                    } else {
-                        current = current.right;
-                    }
-                }
-            }
+        }
+        if (current == null) {
             return false;
         }
+        if (current.left == null && current.right == null) {
+            if (current != root) {
+                if (find.left == current) {
+                    find.left = null;
+                    size--;
+                } else {
+                    find.right = null;
+                    size--;
+                }
+            } else {
+                root = null;
+            }
+        } else if (current.left != null && current.right != null) {
+            Node min = findMinNodeLeft(current.right);
+            int minValue = min.value;
+            remove(minValue);
+            current.value = minValue;
+        } else {
+            Node last = (current.left != null) ? current.left : current.right;
+            if (current != root) {
+                if (current == find.left) {
+                    find.left = last;
+                } else {
+                    find.right = last;
+                }
+            } else {
+                root = last;
+            }
+            size--;
+        }
+        return true;
+    }
+
+    public static Node findMinNodeLeft(Node current) {
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current;
     }
 
     // Should return true if this tree contains specified value, false - otherwise
     public boolean contains(int value) {
         // TODO implement me
         Node current = this.root;
-        Node findNode = new Node();
-        findNode.value = value;
-        if (current.value == findNode.value) {
+        int findValue = value;
+        if (current.value == findValue) {
             return true;
         } else {
             while (current != null) {
                 if (value < current.value) {
                     if (current.left == null) {
                         return false;
-                    }
-                    if (current.left.value == findNode.value) {
+                    } else if (current.left.value == findValue) {
                         return true;
                     } else {
                         current = current.left;
@@ -138,8 +121,7 @@ public class BinaryTree {
                 } else if (value > current.value) {
                     if (current.right == null) {
                         return false;
-                    }
-                    if (current.right.value == findNode.value) {
+                    } else if (current.right.value == findValue) {
                         return true;
                     } else {
                         current = current.right;
