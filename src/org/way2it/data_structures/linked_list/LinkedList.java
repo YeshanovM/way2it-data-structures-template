2a9e29da15b51a3ac4231ca8881b70103b0193d1
Cayman <cayman_raw@ukr.net>
2022-05-27 11:21:37 +0300

    Added intermediate solution LinkedList

--- src/org/way2it/data_structures/linked_list/LinkedList.java
+++ src/org/way2it/data_structures/linked_list/LinkedList.java
@@ -15,6 +15,18 @@ public class LinkedList {
     // Should add new value to the end of the list and increment length
     public void add(String value) {
         // TODO implement me
+        Node addNode = new Node();
+        addNode.value = value;
+        if (head == null) {
+            head = addNode;
+        } else {
+            Node thisNode = head;
+            while (thisNode.next != null) {
+                thisNode = thisNode.next;
+            }
+            thisNode.next = addNode;
+        }
+        length++;
     }
 
 
@@ -22,20 +34,69 @@ public class LinkedList {
     // If afterValue is not present in list - do nothing and return false.
     public boolean addAfter(String value, String afterValue) {
         // TODO implement me
-        return false;
+        boolean addAfter = false;
+        Node addNode = new Node();
+        addNode.value = value;
+        Node thisNode = head;
+        while (thisNode.next != null) {
+            if (thisNode.value.equals(afterValue)) {
+                addNode.next = addNode;
+                thisNode.next = addNode;
+                addAfter = true;
+                length++;
+            } else thisNode = thisNode.next;
+        }
+        return addAfter;
     }
 
     // Should return true if value exists in this list, false - otherwise
     public boolean contains(String value) {
         // TODO implement me
-        return false;
+        boolean contains = false;
+        if (head == null) contains = false;
+        else {
+            Node thisNode = head;
+            if (thisNode.next == null) {
+                contains = thisNode.value == value;
+            } else {
+                while (thisNode.next != null) {
+                    if (thisNode.value.equals(value)) {
+                        contains = true;
+                        break;
+                    }
+                    thisNode = thisNode.next;
+                }
+            }
+        }
+        return contains;
     }
 
     // Should remove first occurrence of the specified value from this list and decrement the length
     // If value existed and was removed - return true, false - otherwise
     public boolean remove(String value) {
         // TODO implement me
-        return false;
+        boolean remove = false;
+        if (head == null) remove = false;
+        else {
+            Node thisNode = head;
+            if (thisNode.next == null) {
+                if (thisNode.value.equals(value)) {
+                    thisNode = null;
+                    length--;
+                    remove = true;
+                }
+            } else {
+                while (thisNode.next != null) {
+                    if (thisNode.value.equals(value)) {
+                        thisNode = thisNode.next;
+                        length--;
+                        remove = true;
+                        break;
+                    } else thisNode = thisNode.next;
+                }
+            }
+        }
+        return remove;
     }
 
     public int getLength() {
